1d8802ba543e919b2664dd22d55b7ee286e53171
Ivan Gabaldon <git@inetol.net>
2024-10-26 20:11:26 +0200

    revert logger eol

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -17,25 +17,25 @@ export const logger = {
 
 	error: (...text: unknown[]): void => {
 		if (logLevel >= LogLevels.error) {
-			console.error(colors.gray('[FRONTEND]'), colors.red('[ERROR]'), text.join('\n'));
+			console.error(colors.gray('[FRONTEND]'), colors.red('[ERROR]'), text.join(' '));
 		}
 	},
 
 	warn: (...text: unknown[]): void => {
 		if (logLevel >= LogLevels.warn) {
-			console.warn(colors.gray('[FRONTEND]'), colors.yellow('[WARN]'), text.join('\n'));
+			console.warn(colors.gray('[FRONTEND]'), colors.yellow('[WARN]'), text.join(' '));
 		}
 	},
 
 	info: (...text: unknown[]): void => {
 		if (logLevel >= LogLevels.info) {
-			console.info(colors.gray('[FRONTEND]'), colors.blue('[INFO]'), text.join('\n'));
+			console.info(colors.gray('[FRONTEND]'), colors.blue('[INFO]'), text.join(' '));
 		}
 	},
 
 	debug: (...text: unknown[]): void => {
 		if (logLevel >= LogLevels.debug) {
-			console.debug(colors.gray('[FRONTEND]'), colors.gray('[DEBUG]'), text.join('\n'));
+			console.debug(colors.gray('[FRONTEND]'), colors.gray('[DEBUG]'), text.join(' '));
 		}
 	}
 } as const;
