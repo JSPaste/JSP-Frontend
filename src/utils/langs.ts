a8abf49c7ae314fc819525beaa456c4ced7c5132
Ivan Gabaldon <igabaldon@inetol.net>
2024-12-26 11:35:31 +0100

    add jsx/tsx, fix typescript highlighting

--- src/utils/langs.ts
+++ src/utils/langs.ts
@@ -6,11 +6,14 @@ export const langs = {
 	java: () => import('@codemirror/lang-java').then((lang) => lang.java),
 	javascript: () => import('@codemirror/lang-javascript').then((lang) => lang.javascript),
 	json: () => import('@codemirror/lang-json').then((lang) => lang.json),
+	jsx: () => import('@codemirror/lang-javascript').then((lang) => () => lang.javascript({ jsx: true })),
 	markdown: () => import('@codemirror/lang-markdown').then((lang) => lang.markdown),
 	php: () => import('@codemirror/lang-php').then((lang) => lang.php),
 	python: () => import('@codemirror/lang-python').then((lang) => lang.python),
 	rust: () => import('@codemirror/lang-rust').then((lang) => lang.rust),
-	typescript: () => import('@codemirror/lang-javascript').then((lang) => lang.javascript),
+	tsx: () =>
+		import('@codemirror/lang-javascript').then((lang) => () => lang.javascript({ jsx: true, typescript: true })),
+	typescript: () => import('@codemirror/lang-javascript').then((lang) => () => lang.javascript({ typescript: true })),
 	xml: () => import('@codemirror/lang-xml').then((lang) => lang.xml),
 	yaml: () => import('@codemirror/lang-yaml').then((lang) => lang.yaml)
 } as const;
